01ed957e797ea908ba5d8c774874c1546dd1902f
Jorge Quilcate <quilcate.jorge@gmail.com>
2014-09-02 15:19:21 -0500

    system B

---
+++ SystemB_App/SystemB_Service/src/org/example/package-info.java
@@ -0,0 +1,4 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org",
+                                     elementFormDefault =
+                                     javax.xml.bind.annotation.XmlNsForm.QUALIFIED) package org.example;
+
