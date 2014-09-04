fd9de689652b67503bc7e98542778fea333a1adf
Jorge Quilcate <quilcate.jorge@gmail.com>
2014-09-04 14:31:20 -0500

    Web Service removed

--- SystemB_App/SystemB_Service/src/org/example/package-info.java
+++
@@ -1,4 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org",
-                                     elementFormDefault =
-                                     javax.xml.bind.annotation.XmlNsForm.QUALIFIED) package org.example;
-
